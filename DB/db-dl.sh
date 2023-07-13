62fff1443fe0af8c4e00152bf717e99a4511d7c3
Amin Zibayi <43344360+AminZibayi@users.noreply.github.com>
2023-07-13 19:15:43 +0330

    Fix: update geoip database download link

--- DB/db-dl.sh
+++ DB/db-dl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz
+wget https://cdn.jsdelivr.net/npm/geolite2-city@1.0.0/GeoLite2-City.mmdb.gz
 gunzip GeoLite2-City.mmdb.gz
