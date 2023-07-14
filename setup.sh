467f0c4b8da9df2d00fa59d1e6eb665aac3e9f60
Amin Zibayi <43344360+AminZibayi@users.noreply.github.com>
2023-07-14 21:04:50 +0330

    Feat: add setup script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Clone the repository
+echo "Cloning the repository..."
+git clone https://github.com/AminZibayi/HoneyMap.git
+
+# Navigate to the HoneyMap directory
+cd ./HoneyMap
+
+# Install system dependencies
+echo "Installing system dependencies..."
+sudo apt install python3-pip redis-server;
+
+# Install python requirements
+echo "Installing python dependencies..."
+sudo pip3 install -U -r requirements.txt
+
+# Configure the DB
+echo "Downloading geoip database..."
+cd ./DB
+./db-dl.sh
+cd ..
+
+echo "Setup done!"
